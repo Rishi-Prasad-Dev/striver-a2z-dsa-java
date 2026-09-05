fdf505a86289956c799a06ec6845629a2c6534fc
RISHI PRASAD <rishiprasad022@gmail.com>
2026-09-05 23:52:17 +0530

    Add binary search 1D solutions (Q07-Q09)

---
+++ src/binary_search/one_d/Q07_CountOccurrences.java
@@ -0,0 +1,76 @@
+package binary_search.one_d;
+
+import java.util.Scanner;
+
+public class Q07_CountOccurrences {
+
+    static int firstOccurrence(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static int lastOccurrence(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        int first = firstOccurrence(arr, x);
+
+        if (first == -1) {
+            System.out.println(0);
+        } else {
+            int last = lastOccurrence(arr, x);
+            System.out.println(last - first + 1);
+        }
+    }
+}
---
+++ src/binary_search/one_d/Q09_SearchInRotatedSortedArrayII.java
@@ -0,0 +1,60 @@
+package binary_search.one_d;
+
+import java.util.Scanner;
+
+public class Q09_SearchInRotatedSortedArrayII {
+
+    static boolean search(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            }
+
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target < arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                if (arr[mid] < target && target <= arr[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        System.out.println(search(arr, x));
+    }
+}
